6a57e627f1428d0b17cc7b140ea600d73b07fac8
Carmel <carmelbarkaee032708@gmail.com>
2025-06-26 23:04:35 -0400

    moved to gui app

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <src/window.h>
+#include <windows.h>
 
 auto main(int argc, char** argv) -> int {
     Window window{};
 
     window.run();
     return 0;
+}
+
+int WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+{
+    return main(0, nullptr);
 }
